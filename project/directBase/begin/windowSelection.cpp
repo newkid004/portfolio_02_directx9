f9b51ff093b7d987dd938ab5845b9a4f4e4e72a9
newkid004 <newkid004@gmail.com>
2019-02-07 20:06:55 +0900

    수정 : windowSelection ( resize ) - scale, size 정규화

--- project/directBase/begin/windowSelection.cpp
+++ project/directBase/begin/windowSelection.cpp
@@ -46,11 +46,15 @@ void windowSelection::drawUI(void)
 
 void windowSelection::resize(int viewX, int viewY)
 {
+	D3DXVECTOR2 texSize; gFunc::getTextureSize(&texSize, _info.backImage);
+
 	_viewCount = D3DXVECTOR2(viewX, viewY);
 
-	_info.size = D3DXVECTOR2(
-		(_bindWindow->getInfo().size.x - BTN_SCROLL_SIZE_X) / _viewCount.x,
-		(_bindWindow->getInfo().size.y - BTN_MOVE_BAR_SIZE_Y) / _viewCount.y);
+	_info.scale = D3DXVECTOR2(
+		((_bindWindow->getInfo().size.x - BTN_SCROLL_SIZE_X) / _viewCount.x) / texSize.x,
+		((_bindWindow->getInfo().size.y - BTN_MOVE_BAR_SIZE_Y) / _viewCount.y) / texSize.y);
+
+	_info.size = gFunc::Vec2Mlt(_info.scale, texSize);
 }
 
 UI_LIST_NODE windowSelection::updateWindow(void)
